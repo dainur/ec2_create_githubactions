e2ba0d9f549430be982bcd494b726cadcb82c876
dainur <157852062+dainur@users.noreply.github.com>
2025-04-06 16:57:19 +0300

    Update main.tf terraform fmt -check

--- infra/main.tf
+++ infra/main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
- region = var.region
+  region = var.region
 }
 
 resource "aws_instance" "my_ec2" {
-    ami  = "ami-03250b0e01c28d196"
-    instance_type = "t2.micro"
+  ami           = "ami-03250b0e01c28d196"
+  instance_type = "t2.micro"
 
-    tags = {
-     Name ="GitHubActionsEC2"
-    }
- }
+  tags = {
+    Name = "GitHubActionsEC2"
+  }
+}
 
 variable "region" {
   default = "eu-central-1"
